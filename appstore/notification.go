4209b06a646862fdc22db37ee08e318ff250355e
Matija Lesar <matija.lesar@gmail.com>
2019-11-25 10:00:13 +0100

    Added unified receipt

--- appstore/notification.go
+++ appstore/notification.go
@@ -9,7 +9,10 @@ const (
 	// Subscription was canceled by Apple customer support.
 	NotificationTypeCancel NotificationType = "CANCEL"
 	// Automatic renewal was successful for an expired subscription.
+	// Deprecated: DID_RECOVER should be used instead of RENEWAL
 	NotificationTypeRenewal NotificationType = "RENEWAL"
+	// Expired subscription recovered through a billing retry.
+	NotificationTypeDidRecover NotificationType = "DID_RECOVER"
 	// Customer renewed a subscription interactively after it lapsed.
 	NotificationTypeInteractiveRenewal NotificationType = "INTERACTIVE_RENEWAL"
 	// Customer changed the plan that takes affect at the next subscription renewal. Current active plan is not affected.
@@ -32,25 +35,39 @@ type NotificationExpiresDate struct {
 }
 
 type NotificationReceipt struct {
-	UniqueIdentifier          string `json:"unique_identifier"`
-	AppItemID                 string `json:"app_item_id"`
-	Quantity                  string `json:"quantity"`
-	VersionExternalIdentifier string `json:"version_external_identifier"`
-	UniqueVendorIdentifier    string `json:"unique_vendor_identifier"`
-	WebOrderLineItemID        string `json:"web_order_line_item_id"`
-	ItemID                    string `json:"item_id"`
-	ProductID                 string `json:"product_id"`
-	BID                       string `json:"bid"`
-	BVRS                      string `json:"bvrs"`
-	TransactionID             string `json:"transaction_id"`
-	OriginalTransactionID     string `json:"original_transaction_id"`
-	IsTrialPeriod             string `json:"is_trial_period"`
-	IsInIntroOfferPeriod      string `json:"is_in_intro_offer_period"`
+	UniqueIdentifier            string `json:"unique_identifier"`
+	AppItemID                   string `json:"app_item_id"`
+	Quantity                    string `json:"quantity"`
+	VersionExternalIdentifier   string `json:"version_external_identifier"`
+	UniqueVendorIdentifier      string `json:"unique_vendor_identifier"`
+	WebOrderLineItemID          string `json:"web_order_line_item_id"`
+	ItemID                      string `json:"item_id"`
+	ProductID                   string `json:"product_id"`
+	BID                         string `json:"bid"`
+	BVRS                        string `json:"bvrs"`
+	TransactionID               string `json:"transaction_id"`
+	OriginalTransactionID       string `json:"original_transaction_id"`
+	IsTrialPeriod               string `json:"is_trial_period"`
+	IsInIntroOfferPeriod        string `json:"is_in_intro_offer_period"`
+	IsUpgraded                  string `json:"is_upgraded"`
+	PromotionalOfferID          string `json:"promotional_offer_id"`
+	SubscriptionGroupIdentifier string `json:"subscription_group_identifier"`
 
 	PurchaseDate
 	OriginalPurchaseDate
 	NotificationExpiresDate
 	CancellationDate
+}
+
+type NotificationUnifiedReceipt struct {
+	Status string `json:"status"`
+
+	// For production we will get "Production" instead of "PROD"
+	Environment NotificationEnvironment `json:"environment"`
+
+	LatestReceipt      string                `json:"latest_receipt"`
+	LatestReceiptInfo  []NotificationReceipt `json:"latest_receipt_info"`
+	PendingRenewalInfo []PendingRenewalInfo  `json:"pending_renewal_info,omitempty"`
 }
 
 type SubscriptionNotification struct {
@@ -79,6 +96,9 @@ type SubscriptionNotification struct {
 	// Not posted for notification_type CANCEL.
 	LatestReceipt     string              `json:"latest_receipt"`
 	LatestReceiptInfo NotificationReceipt `json:"latest_receipt_info"`
+
+	// In the new notifications above properties latest_receipt, latest_receipt_info are moved under this one
+	UnifiedReceipt NotificationUnifiedReceipt `json:"unified_receipt"`
 
 	// Posted only if the notification_type is RENEWAL or CANCEL or if renewal failed and subscription expired.
 	LatestExpiredReceipt     string              `json:"latest_expired_receipt"`
