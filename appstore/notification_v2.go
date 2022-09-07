b9c1d778e67dbfcfd74129b46a24e6c6eb09d461
shaohua <golang@88.com>
2022-09-07 20:52:34 +0800

    add JWSRenewalInfoDecodedPayload.RecentSubscriptionStartDate
    Signed-off-by: shaohua <golang@88.com>

--- appstore/notification_v2.go
+++ appstore/notification_v2.go
@@ -143,18 +143,19 @@ type (
 	// JWSRenewalInfoDecodedPayload contains the decoded renewal information
 	// https://developer.apple.com/documentation/appstoreservernotifications/jwsrenewalinfodecodedpayload
 	JWSRenewalInfoDecodedPayload struct {
-		AutoRenewProductId     string              `json:"autoRenewProductId"`
-		AutoRenewStatus        AutoRenewStatus     `json:"autoRenewStatus"`
-		Environment            Environment         `json:"environment"`
-		ExpirationIntent       ExpirationIntent    `json:"expirationIntent"`
-		GracePeriodExpiresDate int64               `json:"gracePeriodExpiresDate"`
-		IsInBillingRetryPeriod bool                `json:"isInBillingRetryPeriod"`
-		OfferIdentifier        string              `json:"offerIdentifier"`
-		OfferType              OfferType           `json:"offerType"`
-		OriginalTransactionId  string              `json:"originalTransactionId"`
-		PriceIncreaseStatus    PriceIncreaseStatus `json:"priceIncreaseStatus"`
-		ProductId              string              `json:"productId"`
-		SignedDate             int64               `json:"signedDate"`
+		AutoRenewProductId          string              `json:"autoRenewProductId"`
+		AutoRenewStatus             AutoRenewStatus     `json:"autoRenewStatus"`
+		Environment                 Environment         `json:"environment"`
+		ExpirationIntent            ExpirationIntent    `json:"expirationIntent"`
+		GracePeriodExpiresDate      int64               `json:"gracePeriodExpiresDate"`
+		IsInBillingRetryPeriod      bool                `json:"isInBillingRetryPeriod"`
+		OfferIdentifier             string              `json:"offerIdentifier"`
+		OfferType                   OfferType           `json:"offerType"`
+		OriginalTransactionId       string              `json:"originalTransactionId"`
+		PriceIncreaseStatus         PriceIncreaseStatus `json:"priceIncreaseStatus"`
+		ProductId                   string              `json:"productId"`
+		RecentSubscriptionStartDate int64               `json:"recentSubscriptionStartDate"`
+		SignedDate                  int64               `json:"signedDate"`
 	}
 
 	// JWSTransactionDecodedPayload contains the decoded transaction information
